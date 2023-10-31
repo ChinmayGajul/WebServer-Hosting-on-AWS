d76775f0a2f86b2175c60b56ec51face63a89ec1
Chinmay  Gajul <77950645+ChinmayGajul@users.noreply.github.com>
2023-10-31 23:46:44 +0530

    Add files via upload

---
+++ Custom-vsdcvid/css.php
@@ -0,0 +1,116 @@
+/* lightnessfaq faq layout */
+#accordion1.lightnessfaq div.accordionfaqitem
+{
+	margin-top: 0px !important;
+}
+#accordion1.lightnessfaq.contentborder div.accordionfaqitem
+{
+	border-width: 1px 1px 1px 1px;
+	border-style: solid;
+	border-color: #dddddd;
+}
+#accordion1.lightnessfaq.contentborder.headerborder div.accordionfaqitem
+{
+	border-width: 0px 1px 1px 1px;
+}
+#accordion1.lightnessfaq.contentbackground div.accordionfaqitem
+{
+	background: #eeeeee url(../images/ui-bg_highlight-soft_100_eeeeee_1x100.png) 50% top repeat-x;
+	color: #333333;
+}
+#accordion1.lightnessfaq .accordionfaqheader
+{
+	margin: 0px 0px 1px !important;
+}
+#accordion1.lightnessfaq .accordionfaqheader,
+#accordion1.lightnessfaq .accordionfaqheader a
+{
+	font-size: 100%;
+	color: #1c94c4;
+	font-weight: bold;
+	line-height: 24px;
+}
+#accordion1.lightnessfaq.headerborder .accordionfaqheader
+{
+	border: 1px solid #cccccc;
+}
+#accordion1.lightnessfaq.headerborder .accordionfaqheader:hover,
+#accordion1.lightnessfaq.headerborder .accordionfaqheader.selected
+{
+	border: 1px solid #fbd850;
+}
+#accordion1.lightnessfaq.headerbackground .accordionfaqheader,
+#accordion1.lightnessfaq.headerbackground .accordionfaqheader a
+{
+	background: #f6f6f6 url(../images/ui-bg_glass_100_f6f6f6_1x400.png) 50% 50% repeat-x;
+}
+#accordion1.lightnessfaq .accordionfaqheader a,
+#accordion1.lightnessfaq .accordionfaqheader a:hover,
+#accordion1.lightnessfaq .accordionfaqheader a:active,
+#accordion1.lightnessfaq .accordionfaqheader a:focus
+{
+	text-decoration: none;
+	background-color: transparent;
+}
+#accordion1.lightnessfaq .accordionfaqheader:hover,
+#accordion1.lightnessfaq .accordionfaqheader:hover a
+{
+	color: #c77405;
+}
+#accordion1.lightnessfaq .accordionfaqheader.selected,
+#accordion1.lightnessfaq .accordionfaqheader.selected a
+{
+	color: #eb8f00;
+}
+#accordion1.lightnessfaq.headerbackground .accordionfaqheader:hover,
+#accordion1.lightnessfaq.headerbackground .accordionfaqheader:hover a
+{
+	background: #fdf5ce url(../images/ui-bg_glass_100_fdf5ce_1x400.png) 50% 50% repeat-x;
+	color: #c77405;
+}
+#accordion1.lightnessfaq.headerbackground .accordionfaqheader.selected,
+#accordion1.lightnessfaq.headerbackground .accordionfaqheader.selected a
+{
+	background: #ffffff url(../images/ui-bg_glass_65_ffffff_1x400.png) 50% 50% repeat-x;
+	color: #eb8f00;
+}
+#accordion1.lightnessfaq .accordionfaqheader.selected
+{
+	margin-bottom: -1px !important;
+}
+#accordion1.lightnessfaq .accordionfaqheader:hover,
+#accordion1.lightnessfaq .accordionfaqheader.selected
+{
+	background-color: transparent;
+}
+#accordion1.lightnessfaq.rtl.arrow .accordionfaqheader:hover span.accordionfaqicon
+{
+	background-image: url(../images/collapsed-black-l.gif);
+}
+#accordion1.lightnessfaq.arrow .accordionfaqheader:hover span.accordionfaqicon
+{
+	background-image: url(../images/collapsed-black.gif);
+}
+#accordion1.lightnessfaq.arrow .accordionfaqheader.selected span.accordionfaqicon,
+#accordion1.lightnessfaq.rtl.arrow .accordionfaqheader.selected span.accordionfaqicon
+{
+	background-image: url(../images/expanded-black.gif);
+}
+#accordion1.lightnessfaq.defaulticon span.accordionfaqicon
+{
+	background-attachment: scroll;
+	background-image: url(../images/ui-icons_ef8c08_256x240.png);
+	background-repeat: no-repeat;
+	background-position: -112px -192px;
+	width: 16px;
+	height: 16px;
+	overflow: hidden;
+}
+#accordion1.lightnessfaq.rtl.defaulticon span.accordionfaqicon
+{
+	background-position: -144px -192px;
+}
+#accordion1.lightnessfaq.defaulticon .accordionfaqheader.selected span.accordionfaqicon
+{
+	background-position: -128px -192px;
+}
